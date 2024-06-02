adbef1cb19e1e3d08d0f6bcd2fe04fcf8cbfb9dc
Mehzabeen-Ch <mehzabeenmaniar@gmail.com>
2024-06-02 14:46:03 +0100

    Added javascript task to Blog10

---
+++ content/TotalBill.js
@@ -0,0 +1,16 @@
+[{
+
+var a = 142;
+var b = 53;
+
+
+sum = ((a+b)/100);
+tip = ((sum*100 * 0.2) /100);
+
+
+
+
+
+
+alert ('Your Final Total Bill will be:\n Items bill  + tip that is 20% of your item bill');
+}]
